7879ec540d217f6cb2ef4c5572d8211a1ab655f0
accountUnknown <59198999+accountUnknown@users.noreply.github.com>
2019-12-26 13:51:40 +0700

    Add files via upload

---
+++ ReadListInterger.py
@@ -0,0 +1,16 @@
+#Read a list of Integer from user input
+n = int(input("Enter a number of elements: "))
+
+lst = []
+
+for i in range(0,n):
+    num = int(input("Enter a number: "))
+
+    lst.append (num)
+
+
+print( lst)
+
+
+
+            
